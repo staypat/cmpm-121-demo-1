ed9eff8f1776d679459a91ed44add2ad7ba18fef
Patrick Hu <phhu@ucsc.edu>
2024-10-08 00:06:28 -0700

    Step 2 Completed - clicking increases a counter

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,15 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+const counterText = document.createElement("div");
 const button = document.createElement("button");
 button.innerHTML = "Click this ✨";
 app.append(button);
+button.addEventListener("click", () =>{
+    counter++;
+    counterText.innerHTML = `${counter} sparks`;
+    console.log(counter);
+});
+counterText.innerHTML = `${counter} sparks`;
+app.append(counterText)
