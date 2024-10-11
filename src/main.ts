2f51a0048902fe17a5ac14ab72e8da73bb309a8e
Patrick Hu <phhu@ucsc.edu>
2024-10-10 19:37:01 -0700

    Stage 8 Completed - Lore drop

--- src/main.ts
+++ src/main.ts
@@ -21,56 +21,58 @@ button.style.backgroundColor = "transparent";
 button.style.borderRadius = "50%";
 app.append(button);
 button.addEventListener("click", () => {
-    counter++;
-    counterText.innerHTML = `${counter} sparks`;
-    button.style.fontSize = "148.5px";
-    setTimeout(() => {
-        button.style.fontSize = "150px";
-    }, 200);
-    updateButtonState();
+  counter++;
+  counterText.innerHTML = `${counter} sparks`;
+  button.style.fontSize = "148.5px";
+  setTimeout(() => {
+    button.style.fontSize = "150px";
+  }, 200);
+  updateButtonState();
 });
 counterText.innerHTML = `${counter} sparks`;
 passiveGainText.innerHTML = `Sparks per second: ${counterGrowthRate.toFixed(1)}`;
 app.append(counterText);
-app.append(passiveGainText)
+app.append(passiveGainText);
 
 // setInterval(() => {
 //     counter++;
 //     counterText.innerHTML = `${counter} sparks`;
 // }, 1000);
 
-const upgradeButtons: { button: HTMLButtonElement, cost: number }[] = [];
+const upgradeButtons: { button: HTMLButtonElement; cost: number }[] = [];
 
-function addUpgradeButton(name: string, price: number, growthRate: number){
-    let timesPurchased = 0;
-    const growthButton = document.createElement("button");
-    const purchaseText = document.createElement("p");
-    purchaseText.innerHTML = `Times purchased: ${timesPurchased}`;
-    growthButton.innerHTML = `${name} Cost: ${price.toFixed(1)} sparks`;
-    app.append(growthButton);
-    app.append(purchaseText);
-    updateButtonState();
-    upgradeButtons.push({ button: growthButton, cost: price });
-    growthButton.addEventListener("click", () => {
-        if(counter >= price){
-            counterGrowthRate += growthRate;
-            counter -= price;
-            timesPurchased += 1;
-            price *= 1.15;
-            passiveGainText.innerHTML = `Sparks per second: ${counterGrowthRate.toFixed(1)}`;
-            purchaseText.innerHTML = `Times purchased: ${timesPurchased}`;
-            growthButton.innerHTML = `${name} Cost: ${price.toFixed(1)} sparks`;
-            const buttonIndex = upgradeButtons.findIndex(upgrade => upgrade.button === growthButton);
-            upgradeButtons[buttonIndex].cost = price;
-            updateButtonState();
-        }
-    });
+function addUpgradeButton(name: string, price: number, growthRate: number) {
+  let timesPurchased = 0;
+  const growthButton = document.createElement("button");
+  const purchaseText = document.createElement("p");
+  purchaseText.innerHTML = `Times purchased: ${timesPurchased}`;
+  growthButton.innerHTML = `${name} Cost: ${price.toFixed(1)} sparks`;
+  app.append(growthButton);
+  app.append(purchaseText);
+  updateButtonState();
+  upgradeButtons.push({ button: growthButton, cost: price });
+  growthButton.addEventListener("click", () => {
+    if (counter >= price) {
+      counterGrowthRate += growthRate;
+      counter -= price;
+      timesPurchased += 1;
+      price *= 1.15;
+      passiveGainText.innerHTML = `Sparks per second: ${counterGrowthRate.toFixed(1)}`;
+      purchaseText.innerHTML = `Times purchased: ${timesPurchased}`;
+      growthButton.innerHTML = `${name} Cost: ${price.toFixed(1)} sparks`;
+      const buttonIndex = upgradeButtons.findIndex(
+        (upgrade) => upgrade.button === growthButton,
+      );
+      upgradeButtons[buttonIndex].cost = price;
+      updateButtonState();
+    }
+  });
 }
 
 function updateButtonState() {
-    upgradeButtons.forEach(({ button, cost }) => {
-        button.disabled = counter < cost;
-    });
+  upgradeButtons.forEach(({ button, cost }) => {
+    button.disabled = counter < cost;
+  });
 }
 // const growthButton = document.createElement("button");
 // growthButton.innerHTML = "10 sparks for +1 automatic growth rate";
@@ -95,12 +97,12 @@ addUpgradeButton("Flicker", 100, 2);
 addUpgradeButton("Glow", 1000, 50);
 
 let lastTime = performance.now();
-requestAnimationFrame(function increaseCounter(){
-    const currentTime = performance.now();
-    const deltaTime = currentTime - lastTime;
-    counter += (deltaTime / 1000) * counterGrowthRate;
-    counterText.innerHTML = `${Math.floor(counter)} sparks`;
-    lastTime = currentTime;
-    updateButtonState();
-    requestAnimationFrame(increaseCounter);
-})
+requestAnimationFrame(function increaseCounter() {
+  const currentTime = performance.now();
+  const deltaTime = currentTime - lastTime;
+  counter += (deltaTime / 1000) * counterGrowthRate;
+  counterText.innerHTML = `${Math.floor(counter)} sparks`;
+  lastTime = currentTime;
+  updateButtonState();
+  requestAnimationFrame(increaseCounter);
+});
